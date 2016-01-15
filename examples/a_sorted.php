3f9d3857680e9140f7efbbaa27f77b329e3a4818
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2016-01-16 00:02:35 +0100

    Updated examples for nspl\a\sorted

--- examples/a_sorted.php
+++ examples/a_sorted.php
@@ -17,41 +17,50 @@ display('1. Sorted array', $sorted);
 $sortedDesc = sorted([3, 1, 2], true);
 display('2. Sorted in descending order', $sortedDesc);
 
-// 3. Sort array with a given function (order words by length)
+// 3. Sort array by the result of a given function (order words by length)
 $sortedByLength = sorted(['bc', 'a', 'abc'], false, 'strlen');
 display('3. Sorted by length', $sortedByLength);
 
-// 4. Sort array with user-defined function (order word by the 1st character)
-$sortedByTheFirstCharacter = sorted(['bc', 'a', 'abc'], false, null, function($v1, $v2) {
-    return chr($v1[0]) - chr($v2[0]);
-});
+// 4. Sort array by the result of user-defined function (order word by the 1st character)
+$sortedByTheFirstCharacter = sorted(['bc', 'a', 'abc'], false, null, function($v) { return $v[0]; });
 display('4. Sorted by the 1st character', $sortedByTheFirstCharacter);
 
 // 5. Which is the same as
 $sortedByTheFirstCharacter = sorted(['bc', 'a', 'abc'], false, itemGetter(0));
 display('5. Sorted by the 1st character with nspl\op\itemGetter', $sortedByTheFirstCharacter);
 
-// 6. Sort multidimensional array (sort list of users by their names)
+// 6. Sort array with comparison function (order word by the 1st character)
+$sortedByTheFirstCharacter = sorted(['bc', 'a', 'abc'], false, null, function($v1, $v2) {
+    return chr($v1[0]) - chr($v2[0]);
+});
+display('6. Sorted by the 1st character with a comparison function', $sortedByTheFirstCharacter);
+
+// 7. Sort list of strings lexicographically
+$sortedLexicographically = sorted(['bc', 'a', 'abc'], false, null, 'strcmp');
+display('7. Sorted lexicographically', $sortedLexicographically);
+
+// 8. Sort multidimensional array (sort list of users by their names)
 $users = [
     array('name' => 'Robert', 'age' => 20),
     array('name' => 'Alex', 'age' => 30),
     array('name' => 'Jack', 'age' => 25),
 ];
 $sortedByName = sorted($users, false, itemGetter('name'));
-display('6. Users sorted by name', $sortedByName);
+display('8. Users sorted by name', $sortedByName);
 
-// 7. Sort list of objects by property value (sort list of users by their name)
+// 9. Sort list of objects by property value (sort list of users by their name)
 $users = [
     new User('Robert', 20),
     new User('Alex', 30),
     new User('Jack', 25),
 ];
 $sortedByName = sorted($users, false, propertyGetter('name'));
-display('7. Users presented as list of objects sorted by name', $sortedByName);
+display('9. Users presented as list of objects sorted by name', $sortedByName);
 
-// 8. Sort list of objects by property value (sort list of users by their name)
+// 10. Sort list of objects by property value (sort list of users by their name)
 $sortedByAge = sorted($users, false, methodCaller('getAge'));
-display('8. Users presented as list of objects sorted by age', $sortedByAge);
+display('10. Users presented as list of objects sorted by age', $sortedByAge);
+
 
 class User
 {

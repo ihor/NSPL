7c04fbd982db2c9bb9217341fab828e373c46bd8
Ihor Burlachenko <ihor.burlachenko@gmail.com>
2016-02-16 23:03:17 +0100

    Added \nspl test

---
+++ tests/NsplTest/NsplTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace NsplTest;
+
+use function \nspl\getType;
+
+class NsplTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetType()
+    {
+        $this->assertEquals('NULL', getType(null));
+        $this->assertEquals('boolean', getType(true));
+        $this->assertEquals('integer', getType(1));
+        $this->assertEquals('double', getType(1.0));
+        $this->assertEquals('array', getType([]));
+        $this->assertEquals('string', getType('hello world'));
+        $this->assertEquals('stdClass', getType(new \StdClass()));
+    }
+
+}
